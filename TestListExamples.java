0b1c913f88becc9eff7d62614191ec790db68548
Aure361 <114559967+Aure361@users.noreply.github.com>
2022-11-27 18:16:46 -0800

    update

--- TestListExamples.java
+++ TestListExamples.java
@@ -42,3 +42,5 @@ public class TestListExamples {
 	  assertEquals(expected, ListExamples.merge(result1, result2));
   }
 }
+//javac -cp .:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar *.java
+//java -cp .:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples
--- grade.sh
+++ grade.sh
@@ -17,16 +17,16 @@ fi
 cp TestListExamples.java student-submission/
 
 cd student-submission/
-javac -cp .:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar *.java
+javac -cp ".;../lib/hamcrest-core-1.3.jar;../lib/junit-4.13.2.jar" *.java
 if [[ $? -eq 0 ]]
 then
 	echo "ListExamples.java  compiled"
-java -cp .:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples
+java -cp ".;../lib/junit-4.13.2.jar;../lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore TestListExamples
 if [[ $? -eq 0 ]] 
 then 
 	echo "ListExamples.java passed all tests"
 else 
-	echo "java -cp .:../lib/hamcrest-core-1.3.jar../:lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples > output.txt"
+	echo "java -cp .;../lib/junit-4.13.2.jar;../lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore TestListExamples > output.txt"
 	exit 1
 fi
 
