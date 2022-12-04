7557a71f7ea38e00e61da8152d387f20b2a1d598
Aure361 <114559967+Aure361@users.noreply.github.com>
2022-12-03 18:04:19 -0800

    w

--- grade.sh
+++ grade.sh
@@ -3,13 +3,13 @@
 # set -e
 
 rm -rf student-submission
-git clone $1 student-submission
+git clone $1 student-submission > out5.txt 2> err5.txt
 
 echo "In student-submission"
 
 if [[ -e student-submission/ListExamples.java ]]
 then
-	echo "ListExamples.java found"
+	echo "ListExamples.java found" > out6.txt 2> err6.txt
 else
 	echo "ListExamples.java not found"
 fi
@@ -18,19 +18,19 @@ cp TestListExamples.java student-submission/
 
 cd student-submission/
 javac -cp ".;../lib/hamcrest-core-1.3.jar;../lib/junit-4.13.2.jar" *.java
-if [[ $? -eq 0 ]]
+if [[ $? -eq 0 ]] 
 then
 	echo "ListExamples.java  compiled"
 java -cp ".;../lib/junit-4.13.2.jar;../lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore TestListExamples
-if [[ $? -eq 0 ]] 
-then 
+if [[ $? -eq 0 ]]
+then
 	echo "ListExamples.java passed all tests"
-else 
+else
 	echo "java -cp .;../lib/junit-4.13.2.jar;../lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore TestListExamples > output.txt"
 	exit 1
 fi
 
-else 
+else
 	echo "ListExamples.java did not compiled"
 	exit 1
 fi
